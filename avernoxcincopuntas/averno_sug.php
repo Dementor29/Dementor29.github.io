59dcc29d3463af00255c15ff7ac5aec102a718f2
Dementor29 <115649562+Dementor29@users.noreply.github.com>
2025-07-11 22:21:05 -0500

    Update averno_sug.php

--- avernoxcincopuntas/averno_sug.php
+++ avernoxcincopuntas/averno_sug.php
@@ -17,7 +17,7 @@ $stmt->bind_param('ss', $email, $mensaje);
 
 if($stmt->execute()) {
   // Redirigir a página de “gracias” (crea gracias.html)
-  header('Location: avernoxcincopuntas/gracias');
+  header('Location: gracias/index.html');
   exit;
 } else {
   echo "Error al guardar la sugerencia, inténtalo de nuevo.";
